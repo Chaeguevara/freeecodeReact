a5d5bc7463cf013d97c64a5666b951f47674f40f
Chaeguevara <dfso2@naver.com>
2022-04-25 16:42:56 +0900

    Create component
    - define as component for each elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+//traditional JS syntax
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src="https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg" alt="" />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Atomic Habits</h1>;
+const Author = () => <h1>James Clear</h1>;
+//const Greeting = () => {
+//return React.createElement("h1", {}, "hello world");
+//};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
