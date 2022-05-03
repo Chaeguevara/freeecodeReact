4cc5f09af7fd31e18c3cee8bd4c42e84a492d95d
Chaeguevara <dfso2@naver.com>
2022-05-03 19:50:57 +0900

    useSate with array example

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //console.log(useState("hello world"));
+  //const value = useState(1)[0];
+  //const handler = useState(1)[1];
+  //console.log(value, handler);
+  const [text, setText] = useState("hello world");
+  const handleClick = () => {
+    if (text === "This is else statement") {
+      setText("This is wonderful");
+    } else {
+      setText("This is else statement");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
