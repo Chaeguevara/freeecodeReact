0f5a77f1cf92927c19f011829107460cfa08e3b3
Chaeguevara <dfso2@naver.com>
2022-05-08 21:43:08 +0900

    useeffect basic

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  //useEffect(() => {
+  //console.log("call useEffect");
+  //if (value > 0) {
+  //document.title = `New Messsages(${value})`;
+  //} // hook can not be conditional
+  //});
+  console.log("render comp");
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value > 0) {
+      document.title = `New Messsages(${value})`;
+    }
+  }, [value]);
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        {" "}
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
